7b4f2f69d0fb1ef2760895a4d0b41a8e7af7ee37
altafahmad623 <altaf623k300atm@gmail.com>
2021-04-12 06:47:20 +0530

    Done upto 5 april

--- ftbs2.cpp
+++ ftbs2.cpp
@@ -3,17 +3,17 @@ using namespace std;
 #define lli long long
 #define PI 3.14159265358
 // \frac
-// mu = 1/2
+// mu = 0.1
 double fin(double x, double t)
 {
     return 0.0;
 }
 int main()
 {
-    double h = 0.25; // delta x
-    double dt = (1 / 8.0), dx = h;
-    double r = 0.5, mu = 0.5;
-    //double dt = r * (h * h) / (mu);
+    double h = 0.1; // delta x
+    double u2 = 0.1, mu = 0.1;
+    double dt = ((mu * h) / u2), dx = h;
+
     double x_0 = 0.0;
     double x_n = 1.0;
     int N = (x_n - x_0) / h + 0.5;
@@ -24,40 +24,40 @@ int main()
     double *tt = new double[200];
     tt[0] = 0.0;
     xx[0] = x_0;
-    col.push_back((x_0 * x_0));
+    col.push_back((200 * x_0));
     for (int i = 1; i <= N; i++)
     {
         xx[i] = xx[i - 1] + h;
-        if(xx[i] <= 1.01)
-            col.push_back((xx[i] * xx[i]));
+        if (xx[i] <= 0.5)
+            col.push_back((xx[i] * 200));
         else
-            col.push_back(0.0);
+            col.push_back(200 * (1 - xx[i]));
     }
     for (int i = 1; i <= 2 * N; i++)
     {
         tt[i] = tt[i - 1] + dt;
     }
     //cout<<"dfsfsf";
-    
+
     u.push_back(col);
-    for (int n = 1; n <= 2* N; n++)
+    for (int n = 1; n <= 2 * N; n++)
     {
         col[0] = 0;
         for (int i = 1; i <= N; i++)
         {
-            col[i] = (1- mu)*u[n-1][i] + mu*u[n-1][i-1];
+            col[i] = ((1 - mu) * u[n - 1][i]) + (mu * u[n - 1][i - 1]);
         }
         u.push_back(col);
     }
-    
+
     cout << "n -> |";
-    for (int j = 0; j < N * 2; j++)
+    for (int j = 0; j < N; j++)
     {
         printf("\t %d", j);
     }
     printf("\n");
     cout << "t =  |";
-    for (int j = 0; j < N * 2 ; j++)
+    for (int j = 0; j < N; j++)
     {
         printf("\t%0.5lf", tt[j]);
     }
@@ -70,9 +70,9 @@ int main()
     for (int j = 0; j <= N; j++)
     {
         cout << "x_" << j << "  |";
-        for (int i = 0; i < N*2 ; i++)
+        for (int i = 0; i < N; i++)
         {
-            printf("\t%0.5lf", u[i][j]);
+            printf("\t%0.3lf", u[i][j]);
         }
         cout << "\n";
     }
